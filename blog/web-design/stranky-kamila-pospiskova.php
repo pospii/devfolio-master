c342c9dc962f5dd414429252e1d20865baade1cf
Adam Pospíšek <adam.pospisek@email.cz>
2022-01-10 18:05:31 +0100

    new blog

---
+++ blog/web-design/stranky-kamila-pospiskova.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Blog Adam Pospíšek</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="description" content="Blog Adam Pospíšek"/>
+  <meta name="keywords" content="portfolio, programátor, Pospi" />
+  <meta name="author" content="Pospi" />
+  <link rel="shortcut icon" href="../../img/ikona.ico" />
+
+  <!-- Bootstrap CSS File -->
+  <link href="../../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="../../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../../lib/animate/animate.min.css" rel="stylesheet">
+  <link href="../../lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="../../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../../lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="../../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top">Pospik</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../../index.php">Domů</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../../index.php#about">O mně</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../../index.php#skill">Skill</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../../index.php#reference">Reference</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../../index.php#blog">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="../../index.php#contact">Kontakt</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--/ Nav End /-->
+
+  <!--/ Intro Skew Star /-->
+  <div class="intro intro-single route bg-image" style="background-image: url(img/overlay-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <h2 class="intro-title mb-4">Kamila Pospíšková</h2>
+          <ol class="breadcrumb d-flex justify-content-center">
+            <li class="breadcrumb-item">
+              <a href="http://pospi.borec.cz/blog.php">Domů</a>
+            </li>
+            <li class="breadcrumb-item">
+              <a href="http://pospi.borec.cz/blog/web-design.php">Web design</a>
+            </li>
+            <li class="breadcrumb-item active">Kamila Pospíšková</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Intro Skew End /-->
+
+  <!--/ Section Blog-Single Star /-->
+  <section class="blog-wrapper sect-pt4" id="blog">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="post-box">
+            <div class="post-thumb">
+              <img src="../../img/work-11.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="post-meta">
+              <h1 class="article-title">Mgr. Kamila Pospíšková</h1>
+              <ul>
+                <li>
+                  <span class="ion-ios-person"></span>
+                  <a href="http://pospi.borec.cz/">Adam Pospíšek</a>
+                </li>
+                <li>
+                  <span class="ion-pricetag"></span>
+                  <a href="http://pospi.borec.cz/blog/web-design.php">Web Design</a>
+                </li>
+              </ul>
+            </div>
+            <div class="article-content">
+              <p>
+                <a href="http://www.kamilapospiskova.cz/" target="_blank">Webové stránky Kamila Pospíšková</a>
+              </p>
+              <blockquote class="blockquote">
+                <p class="mb-0">Pracuji jako konzultantka, metodička, supervizorka, auditorka a školitelka v oblasti sociálních služeb. Zabývám se kvalitou v sociálních službách. Působím v Moravskoslezském kraji.</p>
+              </blockquote>
+              <div class="post-thumb">
+              <br><img src="../../img/work-55.jpg" class="img-fluid" alt="">
+            </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="widget-sidebar">
+            <h5 class="sidebar-title">Další články</h5>
+            <div class="sidebar-content">
+              <ul class="list-sidebar">
+                <li>
+                  <a href="http://pospi.borec.cz/blog/web-design/stranky-kamila-pospiskova.php">Stránky Kamila Pospíšková</a>
+                </li>
+                <li>
+                  <a href="#">Ve vývoji...</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="widget-sidebar widget-tags">
+            <h5 class="sidebar-title">Tagy</h5>
+            <div class="sidebar-content">
+              <ul>
+                <li>
+                  <a href="http://pospi.borec.cz/blog/web-design.php">Web design</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Section Blog-Single End /-->
+
+  <!--/ Section Contact-Footer Star /-->
+  <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/overlay-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="copyright-box" style="color: transparent; font-size: 0;">
+              <p class="copyright">&copy; Copyright <strong>DevFolio</strong>. All Rights Reserved</p>
+              <div class="credits">
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              </div>
+            </div>
+            <div class="copyright-box">
+              <p class="copyright"><strong>&copy; Adam Pospíšek 2022</strong></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </footer>
+  </section>
+  <!--/ Section Contact-footer End /-->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- JavaScript Libraries -->
+  <script src="../../lib/jquery/jquery.min.js"></script>
+  <script src="../../lib/jquery/jquery-migrate.min.js"></script>
+  <script src="../../lib/popper/popper.min.js"></script>
+  <script src="../../lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../../lib/easing/easing.min.js"></script>
+  <script src="../../lib/counterup/jquery.waypoints.min.js"></script>
+  <script src="../../lib/counterup/jquery.counterup.js"></script>
+  <script src="../../lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="../../lib/lightbox/js/lightbox.min.js"></script>
+  <script src="../../lib/typed/typed.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="../../contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="../../js/main.js"></script>
+
+</body>
+</html>
